01fc20b93ef91038391b2a1b5b8206a071f6d813
VV1102 <105758475+VV1102@users.noreply.github.com>
2022-07-19 14:10:04 -0400

    Create Cartomancy.java

---
+++ Cartomancy.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Cartomancy
+{
+    public static void main(String[] args)
+    {
+        Game cartomancy = new Game();
+       //int times = 0;
+      // while(times>=0)
+       System.out.println("🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮");
+       System.out.println("🔮 Welcome to Cartomancy!🔮");
+       System.out.println("🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮 🔮");
+       System.out.println();
+       cartomancy.giveFortune();
+       //times++;
+      
+    }
+}
